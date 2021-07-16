d97edb8c0813629d40ef8d0428620248c1b47588
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-16 20:51:13 +0300

    Add shingle and ungram

--- src/ngram.test.ts
+++ src/ngram.test.ts
@@ -1,5 +1,23 @@
 import tap from 'tap'
-import { ngram } from '../src/ngram'
+import {
+    ngram,
+    shingle,
+    ungram,
+} from '../src/ngram'
+
+tap.test('shingle', t => {
+    const expected = 'flower'
+
+    t.same(shingle('flowe', 'lower'), expected)
+    t.end()
+})
+
+tap.test('ungram', t => {
+    const expected = 'ngram'
+
+    t.same(ungram(['ngr', 'gra', 'ram']), expected)
+    t.end()
+})
 
 tap.test('ngram works with bigrams', t => {
     const expected = ['ng', 'gr', 'ra', 'am']
--- src/ngram.ts
+++ src/ngram.ts
@@ -1,3 +1,23 @@
+import {
+    concat,
+    init,
+    reduce,
+} from 'rambda'
+
+// Join common end and start parts or just concatenate
+export function shingle (a: string, b: string): string {
+    const common = new RegExp(init(b) + '$')
+    const start = a.replace(common, '')
+
+    return concat(start, b)
+}
+
+// Join ngrams back together
+export function ungram (slices) {
+    return reduce(shingle, '', slices)
+}
+
+// Get ngrams of length n from input
 export function ngram(n: number, input: string) {
     let slices: string[] = []
     let index: number = 0
