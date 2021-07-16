400a38e21d87147c161f9f615b9fc4bb5e1ff755
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-16 19:43:01 +0300

    test: Fix test descriptions

--- src/ngram.test.ts
+++ src/ngram.test.ts
@@ -1,21 +1,21 @@
 import tap from 'tap'
 import { ngram } from '../src/ngram'
 
-tap.test('ngram work with bigrams', t => {
+tap.test('ngram works with bigrams', t => {
     const expected = ['ng', 'gr', 'ra', 'am']
 
     t.same(ngram(2, 'ngram'), expected)
     t.end()
 })
 
-tap.test('ngram work with trigrams', t => {
+tap.test('ngram works with trigrams', t => {
     const expected = ['ngr', 'gra', 'ram']
 
     t.same(ngram(3, 'ngram'), expected)
     t.end()
 })
 
-tap.test('ngram work when n is same as word length', t => {
+tap.test('ngram works when n is word length', t => {
     const expected = ['ngram']
 
     t.same(ngram(5, 'ngram'), expected)
