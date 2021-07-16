3f19fa0c394a4a99be534b5401e440baa8bdc74d
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-16 06:26:54 +0300

    test: Switch to tap and make Typescript and ES modules work on tests
    See ilyadoroshin's pull request for how to make this work:
    https://github.com/tapjs/node-tap/pull/481
    
    They also have a repo with an example config:
    https://github.com/ilyadoroshin/node-tap-ts-example

--- test/ngram.test.ts
+++ test/ngram.test.ts
@@ -1,9 +1,9 @@
-import assert from 'assert'
+import tap from 'tap'
 import { ngrams } from '../src'
 
-describe('ngrams', () => {
-    it('works with bigrams', () => {
-        const expected = ['ng', 'gr', 'ra', 'am']
-        assert.deepStrictEqual(ngrams(2, 'ngram'), expected)
-    })
+tap.test('ngrams work with bigrams', t => {
+    const expected = ['ng', 'gr', 'ra', 'am']
+
+    t.same(ngrams(2, 'ngram'), expected)
+    t.end()
 })
