5350ed2383b04462d16111a1e654724addf0ff4b
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-17 11:22:02 +0300

    test: Shingle with fast check

--- src/ngram.test.ts
+++ src/ngram.test.ts
@@ -1,21 +1,57 @@
+import fc from 'fast-check'
 import tap from 'tap'
+import { init, tail, uniq } from 'rambda'
+
 import {
     ngram,
     shingle,
     ungram,
 } from '../src/ngram'
 
-tap.test('shingle', t => {
-    const expected = 'flower'
+// Shingle
 
-    t.same(shingle('flowe', 'lower'), expected)
+tap.test('shingle', t => {
+    const expected = 'abcdefg'
+
+    t.same(shingle('abcdef', 'efg'), expected)
     t.end()
 })
+
+tap.test('shingle fast check', assert => {
+    assert.doesNotThrow(() => {
+        fc.assert(
+            fc.property(
+                fc.string({minLength: 2}),
+                (
+                    input
+                ) => {
+                    // TODO Check repeating characters!
+                    const word: string = uniq(input.split('')).join('')
+
+                    assert.same(
+                        shingle(init(word), tail(word)),
+                        word
+                    )
+                }))
+    })
+    assert.end()
+})
+
+// Ungram
 
 tap.test('ungram', t => {
     const expected = 'ngram'
 
     t.same(ungram(['ngr', 'gra', 'ram']), expected)
+    t.end()
+})
+
+// Ngram
+
+tap.test('ngram works with unigrams', t => {
+    const expected = ['n', 'g', 'r', 'a', 'm']
+
+    t.same(ngram(1, 'ngram'), expected)
     t.end()
 })
 
@@ -27,9 +63,12 @@ tap.test('ngram works with bigrams', t => {
 })
 
 tap.test('ngram works with trigrams', t => {
-    const expected = ['ngr', 'gra', 'ram']
+    const expected = [
+        'ngra',
+        'gram',
+    ]
 
-    t.same(ngram(3, 'ngram'), expected)
+    t.same(ngram(4, 'ngram'), expected)
     t.end()
 })
 
@@ -38,4 +77,23 @@ tap.test('ngram works when n is word length', t => {
 
     t.same(ngram(5, 'ngram'), expected)
     t.end()
+})
+
+tap.skip('ngram fast check', assert => {
+    assert.doesNotThrow(() => {
+        fc.assert(
+            fc.property(
+                fc.nat(5),
+                fc.string({minLength: 3}),
+                (
+                    n: number,
+                    word: string
+                ) => {
+                    console.log(`ngram word: '${word}'`)
+                    assert.same(ungram(ngram(n, word)), word)
+                }
+            )
+        )
+    })
+    assert.end()
 })
--- src/ngram.ts
+++ src/ngram.ts
@@ -1,20 +1,37 @@
 import {
-    concat,
-    init,
+    head,
+    last,
+    lastIndexOf,
     reduce,
+    splitAt,
+    tail,
 } from 'rambda'
 
 // Join common end and start parts or just concatenate
 export function shingle (a: string, b: string): string {
-    const common = new RegExp(init(b) + '$')
-    const start = a.replace(common, '')
+    const af = lastIndexOf(head(b), a?.split('') || '')
+    const bf = b.indexOf(last(a)) + 1
 
-    return concat(start, b)
+    if (af === -1) return a + b
+    if (bf === -1) return a + b
+
+    const [_, tailA] = splitAt(af, a)
+    const [initB, tailB] = splitAt(bf, b)
+
+    const result = (tailA === initB
+        ? a + tailB
+        : a + b)
+
+    return result
 }
 
 // Join ngrams back together
-export function ungram (slices) {
-    return reduce(shingle, '', slices)
+export function ungram (slices: string[]): string | undefined {
+    if (slices.length === 0) return ''
+
+    const joined = reduce(shingle, head(slices) || '', tail(slices))
+
+    return joined
 }
 
 // Get ngrams of length n from input
