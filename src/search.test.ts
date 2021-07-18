6c021013ec8f7e67850c531403756882a6ea166b
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-18 04:01:47 +0300

    Add Index class with add and all methods

--- src/search.test.ts
+++ src/search.test.ts
@@ -1,6 +1,6 @@
 import tap from 'tap'
 
-import { index } from './search'
+import { Index, index } from './search'
 
 tap.test('index data structure with single item', assert => {
     assert.same(
@@ -10,6 +10,21 @@ tap.test('index data structure with single item', assert => {
             pha: 2,
         },
         index(['alpha'], 3),
+    )
+    assert.end()
+})
+
+tap.test('index class add', assert => {
+    const index = new Index(3)
+    index.add('alpha', 'a')
+
+    assert.same(
+        {
+            alp: {a: 0},
+            lph: {a: 1},
+            pha: {a: 2},
+        },
+        index.all()
     )
     assert.end()
 })
--- src/search.ts
+++ src/search.ts
@@ -4,3 +4,31 @@ import { ngram } from './ngram'
 export function index (terms: string[], n: number = 2) {
     return invert(ngram(n, terms[0]))
 }
+
+type indexable = string | number | symbol
+
+type positions = {
+    [id: string]: string,
+}
+
+export class Index {
+    private terms: Map<indexable, positions>
+
+    constructor (private n: number = 2) {
+        this.n = n
+        this.terms = new Map() // TODO Use WeakMap?
+    }
+
+    add (term, key?: indexable) {
+        const id = key ?? this.terms.size
+        const ngrams = ngram(this.n, term)
+
+        for (let idx in ngrams) {
+            this.terms.set(ngrams[idx], {[id]: idx})
+        }
+    }
+
+    all () {
+        return Object.fromEntries(this.terms.entries())
+    }
+}
