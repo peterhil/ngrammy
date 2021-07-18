33899ba7c114d574821bcc476a7adcc31b6ac244
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-18 04:02:28 +0300

    Remove index function

--- src/search.test.ts
+++ src/search.test.ts
@@ -1,18 +1,6 @@
 import tap from 'tap'
 
-import { Index, index } from './search'
-
-tap.test('index data structure with single item', assert => {
-    assert.same(
-        {
-            alp: 0,
-            lph: 1,
-            pha: 2,
-        },
-        index(['alpha'], 3),
-    )
-    assert.end()
-})
+import { Index } from './search'
 
 tap.test('index class add', assert => {
     const index = new Index(3)
--- src/search.ts
+++ src/search.ts
@@ -1,9 +1,4 @@
-import { invert } from './lib/iterable'
 import { ngram } from './ngram'
-
-export function index (terms: string[], n: number = 2) {
-    return invert(ngram(n, terms[0]))
-}
 
 type indexable = string | number | symbol
 
