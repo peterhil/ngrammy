9f05cd3113aef676941389a042a596b0ad3ecacc
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-26 20:32:04 +0300

    Remove types/ directory

--- types/ngram.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function shingle(a: string, b: string): string;
-export declare function ungram(slices: string[]): string;
-export declare function ngram(n: number, input: string): string[];
-export declare function bigram(input: string): string[];
-export declare function trigram(input: string): string[];
--- types/utils/helpers.d.ts
+++
@@ -1,5 +0,0 @@
-import { Description, Indexable, Position } from '../commonTypes';
-export declare const ids: (obj: Object) => Indexable[];
-export declare const nonEmpty: <T>(x: T) => boolean;
-export declare function positionsAt(id: string, description: Description): Position[];
-export declare function match(candidates: Description, match: Description, pos?: Position): Description;
