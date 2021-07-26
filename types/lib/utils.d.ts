695cdfa4b468a5634eeed404c08d633f93ca6448
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-26 16:08:35 +0300

    types: Update types

---
+++ types/lib/utils.d.ts
@@ -0,0 +1,5 @@
+import { Description, Indexable, Position } from '../types';
+export declare const ids: (obj: Object) => Indexable[];
+export declare const nonEmpty: <T>(x: T) => boolean;
+export declare function positionsAt(id: string, description: Description): Position[];
+export declare function match(candidates: Description, match: Description, pos?: Position): Description;
--- types/search.d.ts
+++ types/search.d.ts
@@ -1,7 +1,7 @@
-import { Description, Indexable, Ngram, NgramIndex, Position, Term } from './types';
+import { Description, Indexable, Ngram, Term } from './types';
 export declare class Index {
     private terms;
-    readonly _normalise: Function;
+    private readonly _normalise;
     readonly n: number;
     readonly sentinel: string;
     constructor(n?: number, sentinel?: string, normalise?: Function);
@@ -14,10 +14,10 @@ export declare class Index {
     locations(term: Term): Description;
     search(term: Term): Indexable[];
     size(): number;
-    _ends(): import("rambda").Dictionary<any>;
-    _get(ngram: Ngram): Description;
-    _getMany(ngrams: Ngram[]): Description[];
-    _checkTermLength(term: Term): Term;
-    _set(ngram: Ngram, value: Description): NgramIndex;
-    _insert(ngram: Ngram, id: Indexable, pos: Position): NgramIndex;
+    private _ends;
+    private _get;
+    private _getMany;
+    private _checkTermLength;
+    private _set;
+    private _insert;
 }
