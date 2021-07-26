20b3a193d706bcca00e3ca0b9ecd02a5d33d4cdb
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-26 11:41:31 +0300

    test: Move helper functions from 'Index.normalise fast check' into testUtils.ts

--- src/search.test.ts
+++ src/search.test.ts
@@ -1,6 +1,5 @@
 import fc from 'fast-check'
 import tap from 'tap'
-import { intersperse } from 'rambda'
 
 import {
     filter,
@@ -10,6 +9,7 @@ import {
 } from 'rambda'
 
 import { Index, match } from './search'
+import { denormalise } from './testUtils'
 
 tap.test('Index.normalise', assert => {
     const term = '  Data\t structures\n '
@@ -23,46 +23,6 @@ tap.test('Index.normalise', assert => {
 })
 
 tap.test('Index.normalise fast check', assert => {
-    const whitespace = [
-        '\u0009', // tab
-        '\u000a', // line feed
-        '\u000b', // line tabulation
-        '\u000c', // form feed
-        '\u000d', // carriage return
-        '\u0020', // space
-        '\u0085', // next line (NEL) Note: \s on regexps does not include this
-        '\u00a0', // no break space
-        '\u1680', // ogham space mark
-        '\u2000', // en quad
-        '\u2001', // em quad
-        '\u2002', // en space
-        '\u2003', // em space
-        '\u2004', // three-per-em space
-        '\u2005', // four-per-em space
-        '\u2006', // six-per-em space
-        '\u2007', // figure space
-        '\u2008', // punctuation space
-        '\u2009', // thin space
-        '\u200a', // hair space
-        '\u2028', // line separator
-        '\u2029', // paragraph separator
-        '\u202f', // narrow no-break space
-        '\u205f', // medium mathematical space
-        '\u3000', // ideographic space
-    ]
-
-    function randomWhitespaceChar() {
-        const idx = Math.floor(Math.random() * whitespace.length)
-        return whitespace[idx]
-    }
-
-    function denormalise(words) {
-        return intersperse(
-            randomWhitespaceChar(),
-            words.split(' ')
-        ).join('')
-    }
-
     fc.assert(
         fc.property(
             fc.lorem(4),
