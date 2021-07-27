84be3e375bd3d58f6813d3d7c7a0b0ad796dbcd6
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-27 20:38:13 +0300

    doc: Add typedoc config

---
+++ typedoc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // entryPoints: [
+    //     'src/search.ts',
+    //     'src/ngram.ts',
+    //     'src/utils/helpers.ts',
+    //     'src/utils/testutils.ts',
+    // ],
+    out: 'docs',
+    // hideGenerator: true,
+    // highlightTheme: 'monokai',
+    includeVersion: true,
+    markedOptions: {
+        baseUrl: __dirname + '/..',
+    },
+    name: 'Ngrammy',
+    readme: 'README.md',
+    theme: 'default',
+    // toc: [
+    //     'Index',
+    //     'ngram',
+    //     'match',
+    // ],
+}
