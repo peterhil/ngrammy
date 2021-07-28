00f89d8a6a02669d3879d8d3e73fa082fe70ba15
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-28 10:57:14 +0300

    config: Fix size-limit

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -1,12 +1,18 @@
 module.exports = [
     {
         limit: "10 KB",
-        path: "dist/ngrammy.cjs.production.min.js",
-        webpack: false,
+        path: "dist/ngrammy.cjs",
     },
     {
         limit: "10 KB",
         path: "dist/ngrammy.esm.js",
-        webpack: false,
+    },
+    {
+        limit: "10 KB",
+        path: "dist/ngrammy.min.cjs",
+    },
+    {
+        limit: "10 KB",
+        path: "dist/ngrammy.esm.min.js",
     },
 ]
